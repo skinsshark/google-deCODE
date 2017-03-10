c33b9bdbe68b0205648b4c49717e1d9507bc6aac
James Fong <James Fong>
2017-03-10 14:11:16 -0500

    Added sign in for google authentication

--- app/src/main/java/com/google_decode/decode_google/MainActivity.java
+++ app/src/main/java/com/google_decode/decode_google/MainActivity.java
@@ -1,5 +1,6 @@
 package com.google_decode.decode_google;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,9 +13,32 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
 
+
+    private static final String TAG = "MainActivity";
+    public static final String ANONYMOUS = "anonymous";
+
+    private String mUsername;
+    private String mPhotoUrl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set default username is anonymous.
+        mUsername = ANONYMOUS;
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        } else {
+            mUsername = mFirebaseUser.getDisplayName();
+            if (mFirebaseUser.getPhotoUrl() != null) {
+                mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+            }
+        }
     }
 }
