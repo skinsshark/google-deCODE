48a6dae6cb950b542854d41db7166f474ff06da9
nicknagi <nekhilnagia16@gmail.com>
2017-03-10 16:32:23 -0500

    Added Comments to Method

--- app/src/main/java/com/google_decode/decode_google/MainActivity.java
+++ app/src/main/java/com/google_decode/decode_google/MainActivity.java
@@ -12,16 +12,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
+import java.io.IOException;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -57,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
             // Not signed in, launch the Sign In activity
             startActivity(new Intent(this, SignInActivity.class));
             finish();
-            return;
         } else {
             mUsername = mFirebaseUser.getDisplayName();
             if (mFirebaseUser.getPhotoUrl() != null) {
@@ -104,21 +107,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //Accepts the local file location of the image as well as the name that should be used to store the image on FireBase.
     private void uploadPic(String fileLoc, String fileName){
+        //Setting up FireBase
         StorageReference mStorageRef;
         mStorageRef = FirebaseStorage.getInstance().getReference();
 
+        //Retrieving Local Image URI
         Uri file = Uri.fromFile(new File(fileLoc));
         StorageReference riversRef = mStorageRef.child("/images/"+fileName);
 
-
+        //Uploading the file to FireBase storage
         riversRef.putFile(file)
                 .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                         // Get a URL to the uploaded content
+                        //The url of the file online
                         //noinspection VisibleForTests
                         downloadUrl = taskSnapshot.getDownloadUrl();
+                        mFirebaseAuth = FirebaseAuth.getInstance();
+                        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                        //Uploads the image details to FireBase Database
+                        DatabaseRetriever.sendUri(downloadUrl,mFirebaseUser.getDisplayName());
                         Log.v("ImageUpload",downloadUrl.toString());
                     }
                 })
@@ -127,27 +138,38 @@ public class MainActivity extends AppCompatActivity {
                     public void onFailure(@NonNull Exception exception) {
                         // Handle unsuccessful uploads
                         // ...
+                        Toast.makeText(getApplicationContext(),"Sorry the file wasn't sent!",Toast.LENGTH_LONG).show();
                     }
                 });
 
     }
 
-//    private void downloadPic(){
-//        File localFile = File.createTempFile("images", "jpg");
-//        riversRef.getFile(localFile)
-//                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-//                    @Override
-//                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-//                        // Successfully downloaded data to local file
-//                        // ...
-//                    }
-//                }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception exception) {
-//                // Handle failed download
-//                // ...
-//            }
-//        });
+//    private void downloadPic(String fileName){
+//        File localFile = null;
+//        try {
+//            localFile = File.createTempFile("images", "jpg");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        StorageReference mStorageRef;
+//        mStorageRef = FirebaseStorage.getInstance().getReference();
+//        StorageReference riversRef = mStorageRef.child("/images/"+fileName);
+//        if (localFile != null) {
+//            riversRef.getFile(localFile)
+//                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+//                            // Successfully downloaded data to local file
+//                            // ...
+//                        }
+//                    }).addOnFailureListener(new OnFailureListener() {
+//                @Override
+//                public void onFailure(@NonNull Exception exception) {
+//                    // Handle failed download
+//                    // ...
+//                }
+//            });
+//        }
 //    }
 }
 
