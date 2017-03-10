be15fdae095dd224955ec0dcadcaee94b0150df5
James Fong <James Fong>
2017-03-10 12:34:53 -0500

    Connected to Firebase

--- app/src/main/java/com/google_decode/decode_google/MainActivity.java
+++ app/src/main/java/com/google_decode/decode_google/MainActivity.java
@@ -3,7 +3,14 @@ package com.google_decode.decode_google;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
