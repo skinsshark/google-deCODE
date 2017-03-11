0667db6268cc361930ad8c02f49bd2ce5f2c5397
nicknagi <nekhilnagia16@gmail.com>
2017-03-10 20:18:47 -0500

    Created an asyncronous thread which downloads the image given the url. Images can be uploaded to firebase given the local path and the filename. Data can be saved in the database as well.

--- app/src/main/java/com/google_decode/decode_google/MainActivity.java
+++ app/src/main/java/com/google_decode/decode_google/MainActivity.java
@@ -2,7 +2,9 @@ package com.google_decode.decode_google;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,7 +26,10 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -55,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         mUsername = ANONYMOUS;
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        //uploadPic("/storage/emulated/0/DCIM/Camera/IMG_20170310_151727.jpg" , "IMG_20170310_151727.jpg");
+        uploadPic("/storage/emulated/0/DCIM/Camera/IMG_20170310_151727.jpg" , "IMG_20170310_151727.jpg");
         //Log.v("ImageUpload",downloadUrl.toString());
         if (mFirebaseUser == null) {
             // Not signed in, launch the Sign In activity
@@ -71,8 +76,7 @@ public class MainActivity extends AppCompatActivity {
             final Button button = (Button) findViewById(R.id.btnTesting);
             button.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                   DatabaseRetriever.sendMessage("Working");
-
+                    DatabaseRetriever.sendMessage("Working");
                     DatabaseRetriever.getMessage();
                 }
             });
@@ -128,8 +132,11 @@ public class MainActivity extends AppCompatActivity {
                         downloadUrl = taskSnapshot.getDownloadUrl();
                         mFirebaseAuth = FirebaseAuth.getInstance();
                         mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                        downloadPic(downloadUrl.toString());
+                        //downloadPic("IMG_20170310_151727.jpg");
+
                         //Uploads the image details to FireBase Database
-                        DatabaseRetriever.sendUri(downloadUrl,mFirebaseUser.getDisplayName());
+                        //DatabaseRetriever.sendUri(downloadUrl,mFirebaseUser.getDisplayName());
                         Log.v("ImageUpload",downloadUrl.toString());
                     }
                 })
@@ -144,10 +151,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    private void downloadPic(String fileName){
+    private void downloadPic(String src ){
+        //This works using file name which downlaods directly from storage
 //        File localFile = null;
 //        try {
-//            localFile = File.createTempFile("images", "jpg");
+//            File storageDir = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//            Log.v("Downloader" , storageDir.toString());
+//            localFile = File.createTempFile("downloadedImage", "jpg", storageDir);
+//            Log.v("Downloader" , "Local file made");
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
@@ -161,6 +172,7 @@ public class MainActivity extends AppCompatActivity {
 //                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
 //                            // Successfully downloaded data to local file
 //                            // ...
+//                            Log.v("Downloader" , "File downloaded and stored");
 //                        }
 //                    }).addOnFailureListener(new OnFailureListener() {
 //                @Override
@@ -170,7 +182,9 @@ public class MainActivity extends AppCompatActivity {
 //                }
 //            });
 //        }
-//    }
+
+        new DownloadImageAsync(this).execute(src);
+    }
 }
 
 
